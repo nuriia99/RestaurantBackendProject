383ce4d8a361170c10721a09089cbca9c6ddceee
Nuria Anton Calle <nuria.anton@col.vueling.com>
2024-10-07 17:22:06 +0200

    Defining entities classes

--- Restaurants.Domain/Entities/Restaurant.cs
+++ Restaurants.Domain/Entities/Restaurant.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Restaurants.Domain.Entities
 {
-    internal class Restaurant
+    public class Restaurant
     {
+        public int Id { get; set; } 
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Category { get; set; } = default!;
+        public bool HasDelivery { get; set; } = default!;
+
+        public string? ContactEmail { get; set; } 
+        public string? ContactNumber { get; set; } 
+
+        public Address? Address { get; set; }
+        public List<Dish> Dishes { get; set; } = new();
     }
 }

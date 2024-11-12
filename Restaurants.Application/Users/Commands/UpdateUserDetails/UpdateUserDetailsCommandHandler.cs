ecacdbfea003cf34c09a6f9c72ff3d8a68e912a9
Nuria Anton Calle <nuria.anton@col.vueling.com>
2024-11-12 17:29:43 +0100

    added admin role context for authorization

---
+++ Restaurants.Application/Users/Commands/UpdateUserDetails/UpdateUserDetailsCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Exceptions;
+
+namespace Restaurants.Application.Users.Commands.UpdateUserDetails;
+
+public class UpdateUserDetailsCommandHandler(ILogger<UpdateUserDetailsCommandHandler> logger,
+    IUserContext userContext,
+    IUserStore<User> userStore) : IRequestHandler<UpdateUserDetailsCommand>
+{
+    public async Task Handle(UpdateUserDetailsCommand request, CancellationToken cancellationToken)
+    {
+        var user = userContext.GetCurrentUser();
+
+        logger.LogInformation("Updating user: {UserId}, with {@Request}", user!.Id, request);
+
+        var dbUser = await userStore.FindByIdAsync(user!.Id, cancellationToken)
+            ?? throw new NotFoundException(nameof(User), user!.Id);
+
+        dbUser.Nationality = request.Nationality;
+        dbUser.DateOfBirth = request.DateOfBirth;
+
+        await userStore.UpdateAsync(dbUser, cancellationToken);
+    }
+}
